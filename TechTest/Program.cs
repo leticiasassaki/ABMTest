8b8ce8b53e84028ccad4468bc0be23c9849532d1
Leticia Kawamoto <leticiac.0401@gmail.com>
2019-11-19 20:21:08 +0000

    Exercise 1

---
+++ TechTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace TechTest
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Exercise1();
+
+
+        }
+
+        static void Exercise1()
+        {
+            string path = Path.GetFullPath("Documents/EDIFACT.txt");
+
+            TextParser textparser = new TextParser();
+
+            var text = textparser.GetMessage(path);
+            var locList = textparser.GetLOCList(text);
+            var cleanLocList = textparser.CleanLocList(locList);
+            var locElements = textparser.GetLocElements(cleanLocList);
+
+            if (locElements == null)
+            {
+                Console.WriteLine("Your list is null");
+            }
+            else
+            {
+                for (int i = 0; i < locElements.Length; i++)
+                    Console.WriteLine($"LOC elements [{locElements[i]}]");
+            }
+        }
+    }
+}
---
+++ TechTest/TextParser.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TechTest
+{
+    class TextParser
+    {
+        public string[] GetMessage(string path)
+        {
+            try
+            {
+                return File.ReadAllLines(path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public List<string> GetLOCList(string[] text)
+        {
+            try
+            {
+                var loc = (from item in text
+                           where item.StartsWith("LOC")
+                           select item).ToList();
+                return loc;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public List<string> CleanLocList(List<string> loc)
+        {
+            try
+            {
+                var locList =  (from item in loc
+                                select item[0..^1]).ToList();
+                return locList;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public string[] GetLocElements(List<string> loc)
+        {
+            try
+            {
+                var values = (from i in loc
+                              let part = i.Split('+')
+                              let item = part[1] + "," + part[2]
+                              select item).ToList();
+
+                return values.ToArray();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+    }
+}
